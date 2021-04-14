d5b1c80bbb886b2f289c4ff8ccfdb2b23b40ab7a
Karolina Mäkinen <karolina.makinen1@gmail.com>
2021-04-14 13:06:38 +0300

    Muutoksia tehty arvoNumerot.java tiedostoon

--- src/arvoNumerot.java
+++ src/arvoNumerot.java
@@ -2,22 +2,22 @@
 public class arvoNumerot {
 	
 	 public static void myMethod() {
-		 int[] numerot = new int[12]; //Tallennetaan listaan 12 numeroa
+		 int[] numerot = new int[13]; //Tallennetaan listaan 12 numeroa
 		    int randomNum; 
 		    System.out.println("Arvotut numerosi on: ");
 
-		    for (int i = 0; i < 12; i++) {
-		        randomNum = (int) (Math.random() * 24); //Tässä ohjelma valitsee numeron randomisti 1-24 numeron välillä
-		        for (int x = 0; x < i; x++) {
+		    for (int i = 1; i < 13; i++) {
+		        randomNum = (int) (Math.random() * 25); //Tässä ohjelma valitsee numeron randomisti 1-24 numeron välillä
+		        for (int x = 1; x < i; x++) {
 		            if (numerot[x] == randomNum) //Koodi tarkistaa onko toista samanlaista numeroa
 		            	{
-		            	randomNum = (int) (Math.random() * 24); //Jos random numero on sama, numero valitaan uudestaan
+		            	randomNum = (int) (Math.random() * 25); //Jos random numero on sama, numero valitaan uudestaan
 		            	x = -1;
 		            	}
 		            }
 		        numerot[i] = randomNum;
 		        }
-		    for (int i = 0; i < numerot.length; i++)
+		    for (int i = 1; i < numerot.length; i++)
 		    	System.out.print(numerot[i] + " ");
 		    }
 	 }
