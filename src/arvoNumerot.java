19d858fe82935cbd2de28c4c1b26cb82f97faefa
Jenna Laaksovirta <jenna.laaksovirta@edu.turkuamk.fi>
2021-04-21 12:43:08 +0300

    Tiedostot palautettu...

---
+++ src/arvoNumerot.java
@@ -0,0 +1,43 @@
+public class arvoNumerot {
+	/** Arvotaan satunnaiset luvut. */
+	public static int[] lottorivi() {
+		/** @randomNum Luodaan muuttuja.*
+		 * @hyvaksyttyjaLukuja Tallentaa lukujen m‰‰r‰n.
+		 * @randomNum Ohjelma valitsee satunnaisen luvun 1-24 v‰lilt‰ ja tallentaa sen muuttujaan.
+		 * @arvotut Tallennetaan taulukkoon 12 numeroa.
+		 * */
+		
+		int[] arvotut = new int[12];
+		int hyvaksyttyjaLukuja = 0;
+		
+		arvontaKierre:
+		while (hyvaksyttyjaLukuja < 12) {
+			int arvottu = (int) (Math.random() * 24) + 1;
+			
+			/** Tarkistetaan onko luku jo taulukossa. */
+			for (int i = 0; i < arvotut.length; i++) {
+				if (arvotut[i] == arvottu) {
+					/**Continue arvontaKierre jatkaa arvontaKierre-silmukkaa.*/
+					continue arvontaKierre;
+				}
+			}
+		/** Lis‰t‰‰n arvottu luku taulukkoon*/
+		arvotut[hyvaksyttyjaLukuja] = arvottu;
+		hyvaksyttyjaLukuja++;
+		}
+		
+		/** @return Palauttaa taulukon.*/
+		return arvotut;
+		
+		}
+	/** Arvotaan onnenapila. */
+	public static int arvoOnnenapila() {
+		/** Arvotaan onnenapila 1 ja 4 v‰lilt‰.
+		* @onnenapila Tallentaa numeron muuttujaan.
+		* @return Palauttaa onnenapilan arvon. */
+		int onnenapila = (int) (Math.random() * 4) + 1;
+
+		return onnenapila;
+	}
+	 	
+}
---
+++ src/valitseNumerot.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class valitseNumerot {
+	
+		private static final Scanner lukija = new Scanner(System.in);
+		/** K‰ytt‰j‰ valitsee lottorivin */
+		public static int[] lottorivi() {
+			/** Kysyt‰‰n k‰ytt‰j‰lt‰ luvut jotka ovat v‰lilt‰ 1 ja 24,
+			 * jos k‰ytt‰j‰ syˆtt‰‰ numeron joka on jos valittu, tai on pienempi kuin 1 tai suurempi kuin 24,
+			 * kysyt‰‰n k‰ytt‰j‰lt‰ uusi luku.
+			 * @numerot Tallennetaan taulukkoon k‰ytt‰j‰n valitsemat numerot.
+			 * @numero Luodaan muuttuja johon tallennetaan numero.
+			 */
+			int[] numerot = new int[12];
+			int numero;
+			
+			for (int i = 0; i < 12; i++) {
+				do {
+				System.out.println("Syˆt‰ " + (i + 1) + ". numero: ");
+				
+				numero = lukija.nextInt();
+				
+					if (numero <= 0) {
+						System.out.println("Valitse numero joka on suurempi kuin 1: ");
+						numero = lukija.nextInt();
+					}
+					else if (numero > 24) {
+						System.out.println("Valitse numero joka on pienempi kuin 24: ");
+						numero = lukija.nextInt();
+					}
+					
+				} while (numero <= 0 && numero > 24);
+				
+				/** K‰yd‰‰n numerot lista l‰pi ja tarkistetaan ettei numeroa ole jo valittu. */
+				for (int x = 0; x < i; x++) {
+					if (numerot[x] == numero) {
+						System.out.println("Et voi valita numeroa jonka olet jo valinnut.");
+						
+						/** Tulostetaan k‰ytt‰j‰lle numerot jotka ovat jo valittuna.
+						 * Poistetaan tulostuksesta listan tyhj‰t (0) kohdat.*/
+						System.out.print("Valitut numerot: ");
+						for (int j : numerot) {
+							if (j != 0) {
+								System.out.print(j + " ");
+							}
+						}
+						System.out.println("");
+						
+						/** K‰ytt‰j‰ valitsee uuden numeron.*/
+						System.out.println("Valitse uusi numero: ");
+						numero = lukija.nextInt();
+					}
+				}
+				
+				numerot[i] = numero;
+			}
+			/** Tulostetaan k‰ytt‰j‰n valitsemat lottonumerot. */
+			
+			System.out.println("Valitsemasi lottonumerot: ");
+			for (int i : numerot) {
+					System.out.print(i + " ");
+			}
+			System.out.println("");
+			
+			/** @return Palauttaa k‰ytt‰j‰n valitsemat lottonumerot p‰‰ohjelmaan. */
+			return numerot;
+		}
+	}
